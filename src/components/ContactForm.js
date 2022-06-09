3f1f8dd20db914dde5ecb4c44a3dc118b958a558
Mishad5 <abdullahahmedkhan100@gmail.com>
2022-06-09 16:58:42 +0600

    contact form formspree added

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -46,43 +46,45 @@ export default function ContactForm() {
   return (
     <>
       <FormStyle>
-        <div className="form-group">
-          <label htmlFor="name">
-            Your Name
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">
-            Your Email
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">
-            Your message
-            <textarea
-              type="text"
-              id="message"
-              name="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-          </label>
-        </div>
-        <button type="submit">Send</button>
+        <form action="https://formspree.io/f/mbjwbovp" method="POST">
+          <div className="form-group">
+            <label htmlFor="name">
+              Your Name
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">
+              Your Email
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">
+              Your message
+              <textarea
+                type="text"
+                id="message"
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </label>
+          </div>
+          <button type="submit">Send</button>
+        </form>
       </FormStyle>
     </>
   );
